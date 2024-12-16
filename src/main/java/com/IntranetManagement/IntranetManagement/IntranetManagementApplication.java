d120c65b7bbd48c5d8e7b6b247299bbc642dc2d0
KhalilHAJRI <khalil.hajri@esen.tn>
2024-12-16 20:53:55 +0100

    first commit

---
+++ src/main/java/com/IntranetManagement/IntranetManagement/IntranetManagementApplication.java
@@ -0,0 +1,13 @@
+package com.IntranetManagement.IntranetManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntranetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntranetManagementApplication.class, args);
+	}
+
+}
